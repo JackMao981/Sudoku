e988d9cd0f4e6e6bf20d1996399ab44797d8a86b
Yuxiang Mao <ymao@olin.edu>
2021-03-08 20:30:44 -0500

    greedy algorithm does what we expect it to do :(

--- sudoku.py
+++ sudoku.py
@@ -2,13 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-def create_sudoku():
+def create_sudoku(a):
     #creates graph
     G = nx.Graph()
 
     #creates all the nodes
-    for row in range(9):
-        for column in range(9):
+    for row in range(int(a*a)):
+        for column in range(int(a*a)):
             G.add_node((row+1,column+1))
 
     # give nodes attributes.
@@ -23,10 +23,10 @@ def create_sudoku():
             if node[1] == node1[1] and node != node1:
                 G.add_edge(node,node1)
             #creates edges b/w nodes in same box
-            nodex = np.ceil(node[0]/3.0)
-            nodey = np.ceil(node[1]/3.0)
-            node1x = np.ceil(node[0]/3.0)
-            node1y = np.ceil(node[1]/3.0)
+            nodex = np.ceil(node[0]/a)
+            nodey = np.ceil(node[1]/a)
+            node1x = np.ceil(node1[0]/a)
+            node1y = np.ceil(node1[1]/a)
             if nodex == node1x and nodey == node1y and node != node1:
                 G.add_edge(node,node1)
 
@@ -39,19 +39,61 @@ def fill_presets(G, numbers):
         G.nodes[(x,y)]["numbers"] = n
     return G
 
-def solve_sudoku_greedy(G):
-#Then we can use greedy to color the graph. They all have the same degree, so we don't need to order by degre
+def solve_sudoku(G):
+    result = True
+    count = 1
+    # colors.values()
+    # print(colors)
+    # {(x,y):color; (x,y):color}
+    colors = nx.get_node_attributes(G,"numbers")
+    while any(map(lambda x: x == 0, colors.values())): #there exists a node whose attribute "numbers" == 0: #while something uncolored
+        if count > 9:
+            # print("The count is ", count)
+            result = False
+        # node that is empty
+        for (x,y) in colors:
+            if colors[(x,y)] == 0:
+                start = (x,y)
+        G = sud_helper(G, start, count)
+        count += 1
+        colors = nx.get_node_attributes(G,"numbers")
+        # print(colors.values())
+    return G, result
+
+# node, color
+def sud_helper(G):
     for node in G.nodes():
+        if G.nodes[node]["numbers"] == 0:
+            # print("here")
+            G.nodes[node]["numbers"] = check_neighbors(G,node) + 1
+    #print(nx.non_neighbors(G,list(G.nodes[node])))
+    return G
 
 
+# return list of not neighbors
+def not_neighbor(G, node):
+    s = list(set(G.nodes()) - set(G.neighbors(node)))
+    s.remove(node)
+    return s
 
+# check if the neighbors are colored. return highest color
+def check_neighbors(G,node):
+    # print(list(G.neighbors(node)))
+    # print(list(map(lambda x: G.nodes[x]["numbers"],list(G.neighbors(node)))))
+    return max(list(map(lambda x: G.nodes[x]["numbers"],list(G.neighbors(node)))))
 
+def check_sudoku(G):
+    pass
 
-
-
-
+# def print_sudoku(G):
+#     for n in G.nodes()
+#         return
 if __name__ == "__main__":
-    G = create_sudoku()
-    solve_sudoku(G)
+    G = create_sudoku(3.0)
+
+    G = sud_helper(G)
+    print(nx.get_node_attributes(G, "numbers"))
+    # print(list(G.nodes()))
+    # G, r = solve_sudoku(G)
     # nx.draw(G, with_labels = True)
     # plt.show()
